59f00fa92b574232a905004379bd27ca6d13daae
Minesh Kumar <mineshkumar1495@gmail.com>
2024-12-10 22:57:05 -0600

    Initial Commit

---
+++ Weights Tracker/Weights_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weights_TrackerApp.swift
+//  Weights Tracker
+//
+//  Created by Minesh Kumar on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Weights_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weights TrackerTests/Weights_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weights_TrackerTests.swift
+//  Weights TrackerTests
+//
+//  Created by Minesh Kumar on 12/10/24.
+//
+
+import Testing
+@testable import Weights_Tracker
+
+struct Weights_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
